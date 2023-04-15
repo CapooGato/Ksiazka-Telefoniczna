ea2ff6af0f4cb984981b0a8ccfe54478c162d907
Semwerynek <74494305+Semwreymek@users.noreply.github.com>
2023-04-15 21:16:52 +0200

    Update Lista.h

--- Lista.h
+++ Lista.h
@@ -1,6 +1,7 @@
 #pragma once
-
+//max dlugosc danych jaka mozna wpisac 
 #define MAX_DLUGOSC 25
+//max liczba kontaktow w ksiazce telefonicznej
 #define MAX_KONTAKTOW 10
 typedef struct {
 	int id, wiek;
@@ -17,10 +18,12 @@ typedef struct {
 	struct kontakty* next;
 }Kontakty;
 
+//Deklarujemy tablice kontaktow, ułatwia to znacznie prace nad projektem, rozmiar tutaj to 10, bo w fazie testowania nie ma potrzeby użycia wiekszej
+//w finalnej wersji projektu zostanie to zmienione na przykladowo 9999999, lub w ogole zniesiemy limit.
 Kontakty kontakty[MAX_KONTAKTOW];
 
 extern int ID;
-
+/*Funkcje sa opisane w pliku Lista.c*/
 void remove_contact();
 void save_to_file_all();
 void nowy_kontakt(int wiekk, char* imiee, char* nazwiskoo, char* ulicaa, char* nr_domuu, char* nr_mieszkaniaa, char* kod_pocztowyy,
