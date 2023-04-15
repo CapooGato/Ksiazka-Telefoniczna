7c7f3f308036ce152dbb258f04818c02d6aa6cc9
Semwerynek <74494305+Semwreymek@users.noreply.github.com>
2023-04-15 20:35:01 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -10,15 +10,16 @@ Mozliwe jest, ze bedzie potrzeba dodania "_CRT_SECURE_NO_WARNINGS", bez "" w: pr
 -> edytuj -> definicje preprocesora -> wpisac:  _CRT_SECURE_NO_WARNINGS  */
 int main(int argc, int **argv)
 {
+	/*Reczne dodanie kilku kontaktow*/
 
 	nowy_kontakt(19,"Damian", "Karwat", "Kielce", "62B", "0", "26-065", "Pieksozow", "888111444", "111000222");
-	
 	nowy_kontakt(20,"Seweryn", "Kawecki", "Kielce", "62B", "0", "26-065", "Pieksozow", "999888111", "111000222");
-
 	nowy_kontakt(19, "Magda", "Gesler", "Kielce", "62B", "0", "26-065", "Pieksozow", "999111888", "000999888");
 	nowy_kontakt(20, "Kapitan", "Tytus", "Kielce", "62B", "0", "26-065", "Pieksozow", "222333444", "555666777");
+	
 	save_to_file_all();
 
+	//Menu
 	menu();
 
 	/*Zabawa z dzialaniem allegro, ktore jest na drugi kamien milowy*/
